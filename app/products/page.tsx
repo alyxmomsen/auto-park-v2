83b6fbc8d16a3d9eded33e3fc2bf1856fa37cef4
alex <shizofranky@gmail.com>
2024-06-03 08:27:23 +0300

    refactor app/globals.css ; add file db/model/main.json ;

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,7 +4,15 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>
+    <div className={`page-base`}>
+      <div className={`product`}>
+        <MyImage 
+          url='https://i.pinimg.com/564x/22/5e/38/225e387a10c06a4d79e8ccc13de90731.jpg' 
+        />
+        <MyImage 
+          url='https://school.oblakoz.ru/static/media/heart.58d6694822882d2e5d949c258a95f695.svg' 
+        />
+      </div>
         <h1>Products</h1>
         <div>content</div>
     </div>
@@ -12,3 +20,14 @@ const Products = () => {
 }
 
 export default Products
+
+
+function MyImage ( {url}:{url:string}) {
+  return <img 
+    className='image-wrapper--flex-container--flex-item'
+    src={url}
+    alt='no alt' 
+    width={200} 
+    height={200} 
+  />
+}
