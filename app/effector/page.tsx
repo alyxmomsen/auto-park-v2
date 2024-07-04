b7a532c6169d29fc7ad0c1915e4a90417a4280be
alex <shizofranky@gmail.com>
2024-07-04 18:45:41 +0300

    add dep effector , effector-react

---
+++ app/effector/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Effector</h1>
+            <div>
+              content
+            </div>
+        </div>
+    )
+}
+export default Page ;
